8cb06d176fe7788b615341d8d0a2aa74281ee84b
kryel <mc@kryel.com>
2013-03-12 22:27:59 +0100

    Refactoring and added ActionListener to usernameField and passwordField

--- application/src/client/Login.java
+++ application/src/client/Login.java
@@ -10,8 +10,8 @@ public class Login extends JPanel implements ActionListener {
 	private static final int LINE_START = GridBagConstraints.LINE_START;
 	private static final int LINE_END = GridBagConstraints.LINE_END;
 	private JFrame frame;
-	private JLabel userLabel, passwordLabel;
-	private JTextField userField;
+	private JLabel usernameLabel, passwordLabel;
+	private JTextField usernameField;
 	private JPasswordField passwordField;
 	private JButton closeButton, logInButton;
 	private GridBagConstraints c;
@@ -19,7 +19,7 @@ public class Login extends JPanel implements ActionListener {
 	public Login(){
 		initFrame();
 		initPanel();
-		
+
 		frame.pack();
 		frame.setVisible(true);
 	}
@@ -37,9 +37,9 @@ public class Login extends JPanel implements ActionListener {
 		c = new GridBagConstraints();
 		setLayout(new GridBagLayout());
 
-		userLabel = new JLabel("Brukernavn");
+		usernameLabel = new JLabel("Brukernavn");
 		passwordLabel = new JLabel("Passord");
-		userField = new JTextField(15);
+		usernameField = new JTextField(15);
 		passwordField = new JPasswordField(15);
 		closeButton = new JButton("Lukk");
 		logInButton = new JButton("Logg inn");
@@ -47,32 +47,37 @@ public class Login extends JPanel implements ActionListener {
 		c.insets = new Insets(0,0,5,0);
 		c.ipadx = 10;
 
-		addComponent(userLabel, LINE_END, 0, 0);
-		addComponent(userField, LINE_START, 1, 0);
-		addComponent(passwordLabel, LINE_END, 0, 1);
-		addComponent(passwordField, LINE_START, 1, 1);
-		addComponent(closeButton, LINE_START, 1, 2); // Hmmm...
-		addComponent(logInButton, LINE_END, 1, 2);
-		
+		addComponent(usernameLabel, 0, 0, LINE_END);
+		addComponent(usernameField, 1, 0, LINE_START);
+		addComponent(passwordLabel, 0, 1, LINE_END);
+		addComponent(passwordField, 1, 1, LINE_START);
+		addComponent(closeButton, 1, 2, LINE_START); // Hmmm...
+		addComponent(logInButton, 1, 2, LINE_END);   // Hmmm...
+
+		usernameField.addActionListener(this);
+		passwordField.addActionListener(this);
 		closeButton.addActionListener(this);
 		logInButton.addActionListener(this);
 	}
 
-	private void addComponent(JComponent component, int anchor, int gridx, int gridy) {
-		c.anchor = anchor;
+	private void addComponent(JComponent component, int gridx, int gridy, int anchor) {
 		c.gridx = gridx;
 		c.gridy = gridy;
+		c.anchor = anchor;
 		add(component, c);
 	}
-	
+
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource().equals(closeButton)) {
+		if (e.getSource().equals(usernameField)) {
+			// TODO
+		} else if (e.getSource().equals(passwordField)) {
+			// TODO
+		} else if (e.getSource().equals(closeButton)) {
 			frame.dispose();
 		} else if (e.getSource().equals(logInButton)) {
 			// TODO logIn
-			frame.dispose(); // If successful? Display error if not successful?
-		} 
-		// TODO Action when return is pressed in userField or passwordField
+			frame.dispose(); // Close if successful; show error message if not?
+		}
 	}
 
 	public static void main(String[] args){
