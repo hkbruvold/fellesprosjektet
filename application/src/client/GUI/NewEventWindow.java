df0bf97e29333f71d80a9b039d98e6d860619b43
kryel <mc@kryel.com>
2013-03-13 13:09:31 +0100

    Initialized and added components to NewEventWindow.java

--- application/src/client/GUI/NewEventWindow.java
+++ application/src/client/GUI/NewEventWindow.java
@@ -1,5 +1,162 @@
 package client.GUI;
 
-public class NewEventWindow {
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+import client.User;
+
+@SuppressWarnings("serial")
+public class NewEventWindow extends JPanel implements ActionListener {
+	private static final String FRAME_NAME = "Avtale / Møte";
+	
+	private static final String LABEL_DESCRIPTION = "Beskrivelse";
+	private static final String LABEL_FROM = "Fra";
+	private static final String LABEL_TO = "Til";
+	private static final String LABEL_LOCATION = "Sted";
+	private static final String LABEL_ALARM = "Alarm";
+	private static final String LABEL_ALARM_TIME_BEFORE = "Tid før:";
+	private static final String LABEL_MEETING = "Møte";
+	private static final String LABEL_PARTICIPANTS = "Deltakere:";
+	
+	private static final String FIELD_DATE = "ÅÅÅÅ-MM-DD";
+	private static final String FIELD_TIME = "TT:MM";
+
+	private static final String BUTTON_DELETE = "Slett";
+	private static final String BUTTON_CLOSE = "Lukk";
+	private static final String BUTTON_SAVE = "Lagre";
+	
+	private static final int SIZE_FIELD = 12;
+	
+	private static final int LINE_START = GridBagConstraints.LINE_START;
+	private static final int CENTER = GridBagConstraints.CENTER;
+	private static final int LINE_END = GridBagConstraints.LINE_END;
+	
+	private JFrame frame;
+	private JLabel descriptionLabel, fromLabel, toLabel, locationLabel, alarmLabel, alarmTimeBeforeLabel, meetingLabel, participantsLabel;
+	private JTextField descriptionField, fromDateField, fromTimeField, toDateField, toTimeField, locationField, alarmTimeBeforeField;
+	private JCheckBox alarmCheckBox, meetingCheckBox;
+	private JList<User> participantsList;
+	private JButton deleteButton, closeButton, saveButton;
+	private GridBagConstraints c;
+
+	public NewEventWindow() {
+		initFrame();
+		initPanel();
+		
+		frame.pack();
+		frame.setVisible(true);
+//		System.out.println(frame.getWidth() + ", " + frame.getHeight());
+	}
+	
+	private void initFrame() {
+		frame = new JFrame(FRAME_NAME);
+		frame.setPreferredSize(new Dimension(300, 400)); // TODO temp
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.getContentPane().add(this);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	private void initPanel() {
+		c = new GridBagConstraints();
+		setLayout(new GridBagLayout());
+
+		initComponents();
+
+		c.insets = new Insets(0,0,5,0);
+		c.ipadx = 10;
+
+		addComponents();
+
+		deleteButton.addActionListener(this);
+		closeButton.addActionListener(this);
+		saveButton.addActionListener(this);
+	}
+	private void initComponents() {
+		descriptionLabel = new JLabel(LABEL_DESCRIPTION);
+		fromLabel = new JLabel(LABEL_FROM);
+		toLabel = new JLabel(LABEL_TO);
+		locationLabel = new JLabel(LABEL_LOCATION);
+		alarmLabel = new JLabel(LABEL_ALARM);
+		alarmTimeBeforeLabel = new JLabel(LABEL_ALARM_TIME_BEFORE);
+		meetingLabel = new JLabel(LABEL_MEETING);
+		participantsLabel = new JLabel(LABEL_PARTICIPANTS);
+
+		descriptionField = new JTextField(SIZE_FIELD);
+		fromDateField = new JTextField(FIELD_DATE);
+		fromTimeField = new JTextField(FIELD_TIME);
+		toDateField = new JTextField(FIELD_DATE);
+		toTimeField = new JTextField(FIELD_TIME);
+		locationField = new JTextField(SIZE_FIELD);
+		alarmTimeBeforeField = new JTextField(FIELD_TIME);
+		
+		alarmCheckBox = new JCheckBox();
+		meetingCheckBox = new JCheckBox();
+		participantsList = new JList<User>(/* TODO */);
+		participantsList.setBackground(Color.WHITE);
+		// temp:
+		participantsList.setListData(new User[]{new User("Ola"), new User("Kari"), new User("Nordmann")});
+		
+		deleteButton = new JButton(BUTTON_DELETE);
+		closeButton = new JButton(BUTTON_CLOSE);
+		saveButton = new JButton(BUTTON_SAVE);
+	}
+	private void addComponents() {
+		addComponent(descriptionLabel, 0, 0, LINE_END);
+		addComponent(descriptionField, 1, 0, LINE_START);
+	
+		addComponent(fromLabel, 0, 1, LINE_END);
+		addComponent(fromDateField, 1, 1, LINE_START);
+		addComponent(fromTimeField, 1, 1, LINE_END);
+		
+		addComponent(toLabel, 0, 2, LINE_END);
+		addComponent(toDateField, 1, 2, LINE_START);
+		addComponent(toTimeField, 1, 2, LINE_END);
+		
+		addComponent(locationLabel, 0, 3, LINE_END);
+		addComponent(locationField, 1, 3, LINE_START);
+		
+		addComponent(alarmLabel, 0, 4, LINE_END);
+		addComponent(alarmCheckBox, 1, 4, LINE_START);
+		addComponent(alarmTimeBeforeLabel, 1, 4, CENTER);
+		addComponent(alarmTimeBeforeField, 1, 4, LINE_END);
+		
+		addComponent(meetingLabel, 0, 5, LINE_END);
+		addComponent(meetingCheckBox, 1, 5, LINE_START);
+		
+		addComponent(participantsLabel, 0, 6, GridBagConstraints.FIRST_LINE_END);
+		addComponent(participantsList, 1, 6, LINE_START);
+		
+		addComponent(deleteButton, 0, 7, LINE_END);
+		addComponent(closeButton, 1, 7, LINE_START);
+		addComponent(saveButton, 1, 7, LINE_END);
+	}
+	private void addComponent(JComponent component, int gridx, int gridy, int anchor) {
+		c.gridx = gridx;
+		c.gridy = gridy;
+		c.anchor = anchor;
+		add(component, c);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+//		if (e.getSource().equals(usernameField)) {
+//			// TODO
+//		} else if (e.getSource().equals(passwordField)) {
+//			// TODO
+		if (e.getSource().equals(closeButton)) {
+			frame.dispose();
+		} else if (e.getSource().equals(saveButton)) {
+			// TODO save new event
+			frame.dispose(); // Close if successful; show error message if not?
+		}
+	}
+	
+	public static void main(String[] args) {
+		new NewEventWindow();
+	}
 
 }
