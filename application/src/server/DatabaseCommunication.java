d1ca12f4f17574ccf461781cd957b93477d1ea33
kryel <mc@kryel.com>
2013-03-14 13:29:11 +0100

    Refactoring in DatabaseCommunication.java

--- application/src/server/DatabaseCommunication.java
+++ application/src/server/DatabaseCommunication.java
@@ -5,46 +5,52 @@ import java.sql.SQLException;
 
 public class DatabaseCommunication {
 	private DatabaseConnection dbConn;
-	// TODO fields?
 	
 	public DatabaseCommunication(DatabaseConnection dbConn) {
 		this.dbConn = dbConn;
 	}
 
-	// TODO
-	
-	public void /* result */ query(String query) {
+	public int query(String query) {
 		try {
-			dbConn.init();
-			ResultSet rs = dbConn.query(query);
-
-			rs.beforeFirst();
-			int colCount = rs.getMetaData().getColumnCount();
-			while (rs.next()) {
-				for (int i = 1; i < colCount+1; i++) {
-					System.out.print(rs.getMetaData().getColumnLabel(i) + ": ");
-					System.out.println(rs.getString(i));
-				}
-				System.out.println();
-			}
-			
-			rs.close();
-			dbConn.close();
+			makeQuery(query);
+			return 0;
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
+			return -1;
 		}
 	}
-	public void /* status */ update(String update) { // insert single element to database
+	public int update(String update) {
 		try {
-			dbConn.init();
-			dbConn.update(update);
-			dbConn.close();
+			makeUpdate(update);
+			return 0;
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
+			return -1;
 		}
 	}
-	public void /* status */ insertBatch(/* TODO */) { // batch insert (multiple items) to database
+	public int batchUpdate(/* TODO */) {
 		// TODO
+		return -2;
 	}
-	// TODO metod(s): change/update
+
+	private void makeQuery(String query) throws ClassNotFoundException, SQLException {
+		dbConn.init();
+		ResultSet rs = dbConn.query(query);
+		rs.beforeFirst();
+		int colCount = rs.getMetaData().getColumnCount();
+		while (rs.next()) {
+			for (int i = 1; i < colCount+1; i++) {
+				System.out.print(rs.getMetaData().getColumnLabel(i) + ": ");
+				System.out.println(rs.getString(i));
+			}
+			System.out.println();
+		}
+		rs.close();
+		dbConn.close();
+	}
+	private void makeUpdate(String update) throws ClassNotFoundException, SQLException {
+		dbConn.init();
+		dbConn.update(update);
+		dbConn.close();
+	}
 }
