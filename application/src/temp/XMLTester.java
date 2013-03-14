d0ec01c74bf9dccfeb18bc642a3d504b6de92651
lapstue <lapstue@gmail.com>
2013-03-14 20:27:47 +0100

    Added a XMLTester class

---
+++ application/src/temp/XMLTester.java
@@ -0,0 +1,10 @@
+package temp;
+
+import temp.TestObjects;
+import client.XMLTranslator;
+
+public class XMLTester {
+	public static void main(String[] args) {
+		XMLTranslator.toXML(TestObjects.getMeeting00());
+	}
+}
