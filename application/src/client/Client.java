b69179a8f1a13070a3e45e592edd733947dc10cb
kryel <mc@kryel.com>
2013-03-15 19:43:00 +0100

    Trying to send and receive from both server and client. Timing is needed? Can only get one of them to receive at the time

--- application/src/client/Client.java
+++ application/src/client/Client.java
@@ -13,11 +13,13 @@ public class Client {
  
         try {
             socket = new Socket(hostadress, port);
+            
             out = new PrintWriter(socket.getOutputStream(), true);
-            System.out.println("Sending...");
-            out.write("Hello world!");
-            System.out.println("Sending done");
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            
+            receive(in);
+            send(out);
+            
         } catch (UnknownHostException e) {
             System.err.println("Could not find host");
             System.exit(1);
@@ -33,4 +35,26 @@ public class Client {
         in.close();
         socket.close();
     }
+
+    private static void send(PrintWriter out) {
+    	System.out.println("Client: Sending start");
+    	out.write("Client says: Hello");
+    	System.out.println("Client: Sending end");
+    }
+	private static void receive(BufferedReader in) throws IOException {
+		System.out.println("Client: Receiving start");
+		String input;
+		if (in.ready()) {
+			input = in.readLine();
+		} else {
+			System.out.println("Client: 'in' is not ready. Returning");
+			return;
+		}
+		while (input != null) {
+			System.out.println(input);
+			input = in.readLine();
+		}
+		System.out.println("Client: Receiving end");
+	}
+
 }
--- application/src/server/ServerThread.java
+++ application/src/server/ServerThread.java
@@ -17,13 +17,10 @@ public class ServerThread extends Thread {
 		try {
 			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			System.out.println("Receiving...");
-			String input = in.readLine();
-			while (input != null) {
-				System.out.println(input);
-				input = in.readLine();
-			}
-			System.out.println("Receiving done");
+			
+			send(out);
+			receive(in);
+
 			out.close();
 			in.close();
 			socket.close();
@@ -31,5 +28,27 @@ public class ServerThread extends Thread {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private void receive(BufferedReader in) throws IOException {
+		System.out.println("Server: Receiving start");
+		String input;
+		if (in.ready()) {
+			input = in.readLine();
+		} else {
+			System.out.println("Server: 'in' is not ready. Returning");
+			return;
+		}
+		while (input != null) {
+			System.out.println(input);
+			input = in.readLine();
+		}
+		System.out.println("Server: Receiving end");
+	}
+
+	private void send(PrintWriter out) {
+		System.out.println("Server: Sending start");
+		out.write("Server says: Hello");
+		System.out.println("Server: Sending end");
 	}
 }
