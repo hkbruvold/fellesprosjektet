70710093be5622ae0c03c3616047e4cb16546166
kryel <mc@kryel.com>
2013-03-17 18:22:28 +0100

    Added class RoomSelectionWindow.java and working on implementing it in NewEventWindow.java

---
+++ application/src/client/GUI/RoomSelectionWindow.java
@@ -0,0 +1,16 @@
+package client.GUI;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class RoomSelectionWindow extends JPanel {
+	
+	public RoomSelectionWindow() {
+		
+	}
+	
+	public static void main(String[] args) {
+		new RoomSelectionWindow();
+	}
+	
+}
