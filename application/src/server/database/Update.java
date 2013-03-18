48e80622e76709504227ca4b879879c070a43f4f
kryel <mc@kryel.com>
2013-03-18 15:59:17 +0100

    Added constructor (and temporary main) method to Insert.java and Update.java

--- application/src/server/database/Update.java
+++ application/src/server/database/Update.java
@@ -1,5 +1,24 @@
 package server.database;
 
+import data.*;
+
+import temp.TestObjects;
+
 public class Update {
+	private User currentUser;
+	private DatabaseCommunication dbComm;
+
+	public Update(User currentUser, DatabaseCommunication dbComm) {
+		this.currentUser = currentUser;
+		this.dbComm = dbComm;
+	}
+
+	// TODO add update methods
+	
+	public static void main(String[] args) {
+		DatabaseConnection dbConn = new DatabaseConnection("jdbc:mysql://localhost:3306/calendarDatabase", "root", "skip".toCharArray());
+		DatabaseCommunication dbComm = new DatabaseCommunication(dbConn);
+		Update update = new Update(TestObjects.getUser00(), dbComm);
+	}
 
 }
