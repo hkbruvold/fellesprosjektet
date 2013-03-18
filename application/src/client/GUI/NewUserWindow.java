74dbb2d252101f58e0927de3e6bb4b57613001c1
lapstue <lapstue@gmail.com>
2013-03-18 16:27:10 +0100

    New user window

---
+++ application/src/client/GUI/NewUserWindow.java
@@ -0,0 +1,147 @@
+package client.GUI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.Box;
+
+import data.User;
+import data.UserTypes;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class NewUserWindow extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField usernameField;
+	private JTextField passwordField;
+	private JTextField nameField;
+	private User user;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					NewUserWindow frame = new NewUserWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public NewUserWindow() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 300, 250);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JLabel lblNewLabel = new JLabel("Username");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 1;
+		gbc_lblNewLabel.gridy = 0;
+		contentPane.add(lblNewLabel, gbc_lblNewLabel);
+		
+		usernameField = new JTextField();
+		GridBagConstraints gbc_usernameField = new GridBagConstraints();
+		gbc_usernameField.insets = new Insets(0, 0, 5, 0);
+		gbc_usernameField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_usernameField.gridx = 3;
+		gbc_usernameField.gridy = 0;
+		contentPane.add(usernameField, gbc_usernameField);
+		usernameField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Password");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 1;
+		gbc_lblNewLabel_1.gridy = 1;
+		contentPane.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		passwordField = new JTextField();
+		GridBagConstraints gbc_passwordField = new GridBagConstraints();
+		gbc_passwordField.insets = new Insets(0, 0, 5, 0);
+		gbc_passwordField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_passwordField.gridx = 3;
+		gbc_passwordField.gridy = 1;
+		contentPane.add(passwordField, gbc_passwordField);
+		passwordField.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Name");
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_2.gridx = 1;
+		gbc_lblNewLabel_2.gridy = 2;
+		contentPane.add(lblNewLabel_2, gbc_lblNewLabel_2);
+		
+		nameField = new JTextField();
+		GridBagConstraints gbc_nameField = new GridBagConstraints();
+		gbc_nameField.insets = new Insets(0, 0, 5, 0);
+		gbc_nameField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_nameField.gridx = 3;
+		gbc_nameField.gridy = 2;
+		contentPane.add(nameField, gbc_nameField);
+		nameField.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("Type");
+		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_3.gridx = 1;
+		gbc_lblNewLabel_3.gridy = 3;
+		contentPane.add(lblNewLabel_3, gbc_lblNewLabel_3);
+		
+		final JComboBox userType = new JComboBox(UserTypes.values());
+		GridBagConstraints gbc_userType = new GridBagConstraints();
+		gbc_userType.insets = new Insets(0, 0, 5, 0);
+		gbc_userType.fill = GridBagConstraints.HORIZONTAL;
+		gbc_userType.gridx = 3;
+		gbc_userType.gridy = 3;
+		contentPane.add(userType, gbc_userType);
+		
+		Box horizontalBox = Box.createHorizontalBox();
+		GridBagConstraints gbc_horizontalBox = new GridBagConstraints();
+		gbc_horizontalBox.insets = new Insets(0, 0, 5, 0);
+		gbc_horizontalBox.gridx = 3;
+		gbc_horizontalBox.gridy = 5;
+		contentPane.add(horizontalBox, gbc_horizontalBox);
+		
+		JButton btnNewButton = new JButton("Save");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//TODO send to database
+				user = new User(usernameField.getText(),nameField.getText(),userType.getSelectedItem().toString());
+			}
+		});
+		horizontalBox.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Exit");
+		horizontalBox.add(btnNewButton_1);
+	}
+
+}
