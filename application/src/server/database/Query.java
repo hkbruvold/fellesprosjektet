214624677f6311a5c386d5f3bdd7278bcf5739f8
kryel <mc@kryel.com>
2013-03-18 18:49:27 +0100

    Reorganized constants

--- application/src/server/database/Query.java
+++ application/src/server/database/Query.java
@@ -8,19 +8,6 @@ import temp.TestObjects;
 import data.*;
 
 public class Query {
-	private static final String FIELDS_ALARM = TableFields.ALARM.getFieldsString();
-	private static final String FIELDS_EVENT = TableFields.EVENT.getFieldsString();
-	private static final String FIELDS_GROUPS = TableFields.GROUPS.getFieldsString();
-	private static final String FIELDS_IS_MEMBER_OF = TableFields.IS_MEMBER_OF.getFieldsString();
-	private static final String FIELDS_IS_OWNER = TableFields.IS_OWNER.getFieldsString();
-	private static final String FIELDS_IS_PARTICIPANT = TableFields.IS_PARTICIPANT.getFieldsString();
-	private static final String FIELDS_NOTIFICATION = TableFields.NOTIFICATION.getFieldsString();
-	private static final String FIELDS_NOTIFICATION_FOR_EVENT = TableFields.NOTIFICATION_FOR_EVENT.getFieldsString();
-	private static final String FIELDS_NOTIFICATION_TO = TableFields.NOTIFICATION_TO.getFieldsString();
-	private static final String FIELDS_RESERVED_ROOM = TableFields.RESERVED_ROOM.getFieldsString();
-	private static final String FIELDS_ROOM = TableFields.ROOM.getFieldsString();
-	private static final String FIELDS_USER = TableFields.USER.getFieldsString();
-
 	private static final String TABLE_ALARM = TableFields.ALARM.getTableName();
 	private static final String TABLE_EVENT = TableFields.EVENT.getTableName();
 	private static final String TABLE_GROUPS = TableFields.GROUPS.getTableName();
@@ -34,6 +21,19 @@ public class Query {
 	private static final String TABLE_ROOM = TableFields.ROOM.getTableName();
 	private static final String TABLE_USER = TableFields.USER.getTableName();
 	
+	private static final String FIELDS_ALARM = TableFields.ALARM.getFieldsString();
+	private static final String FIELDS_EVENT = TableFields.EVENT.getFieldsString();
+	private static final String FIELDS_GROUPS = TableFields.GROUPS.getFieldsString();
+	private static final String FIELDS_IS_MEMBER_OF = TableFields.IS_MEMBER_OF.getFieldsString();
+	private static final String FIELDS_IS_OWNER = TableFields.IS_OWNER.getFieldsString();
+	private static final String FIELDS_IS_PARTICIPANT = TableFields.IS_PARTICIPANT.getFieldsString();
+	private static final String FIELDS_NOTIFICATION = TableFields.NOTIFICATION.getFieldsString();
+	private static final String FIELDS_NOTIFICATION_FOR_EVENT = TableFields.NOTIFICATION_FOR_EVENT.getFieldsString();
+	private static final String FIELDS_NOTIFICATION_TO = TableFields.NOTIFICATION_TO.getFieldsString();
+	private static final String FIELDS_RESERVED_ROOM = TableFields.RESERVED_ROOM.getFieldsString();
+	private static final String FIELDS_ROOM = TableFields.ROOM.getFieldsString();
+	private static final String FIELDS_USER = TableFields.USER.getFieldsString();
+
 	private static final String BIT_FALSE = "0";
 	private static final String BIT_TRUE = "1";
 
