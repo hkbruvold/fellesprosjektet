885235fdbe8553285a52338a06e44ce4ebe65913
Hans-Kristian Bruvold <hkbruvold@gmail.com>
2013-03-20 04:56:37 +0100

    Added better colour support

--- application/src/client/GUI/EventComponent.java
+++ application/src/client/GUI/EventComponent.java
@@ -12,36 +12,43 @@ import data.Event;
 public class EventComponent extends JComponent { //TODO find proper component
 	private Event event;
 	
-	private int borderWidth = 2; //Border width in pixels
+	private int borderWidth = 1; //Border width in pixels
 	private int[] RGBColor = {45, 60, 255};
-	private Color eventColor = new Color(RGBColor[0], RGBColor[1], RGBColor[2]);
+	private int[] RGBBGColor = {0, 0, 155};
 
 	public EventComponent(Event event) {
 		this.event = event;
+		setColor(new int[]{45, 60, 255});
 	}
 	
 	protected void paintComponent(Graphics g) {
-		if (isOpaque()) {
-			g.setColor(Color.black);
-			g.fillRect(0, 0, getWidth(), getHeight());
-		}
+		g.setColor(new Color(RGBBGColor[0], RGBBGColor[1], RGBBGColor[2]));
+		g.fillRect(0, 0, getWidth(), getHeight());
 		
-		g.setColor(eventColor);
-		g.fillRect(borderWidth, borderWidth, getWidth()-borderWidth, getHeight()-borderWidth);
+		g.setColor(new Color(RGBColor[0], RGBColor[1], RGBColor[2]));
+		g.fillRect(borderWidth, borderWidth, getWidth()-2*borderWidth, getHeight()-2*borderWidth);
 		
 		g.setColor(Color.black);
-		g.drawString(event.getDescription(), 10, 10);
+		g.drawString(event.getDescription(), 2, 10);
 	}
 	
 	public void setColor(int[] RGBColor) {
 		if (RGBColor.length != 3) {
 			return;
 		}
-		for (int c: RGBColor) {
-			if (c < 0 || c > 255) {
+		
+		for (int i = 0; i < 3; i++) {
+			if (RGBColor[i] < 0 || RGBColor[i] > 255) {
 				return;
 			}
+			
+			int bgc = RGBColor[i] - 100;
+			if (bgc < 0) {
+				bgc = 0;
+			}
+			RGBBGColor[i] = bgc;
 		}
+		
 		this.RGBColor = RGBColor;
 	}
 
